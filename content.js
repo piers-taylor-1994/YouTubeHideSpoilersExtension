4c1a0a6d618d6697e0c6618234f1dcc1da8f7956
Piers Taylor <piers.taylorr@gmail.com>
2024-02-05 17:10:42 +0000

    Works only on button press (no local storage used)

--- content.js
+++ content.js
@@ -1,17 +1,41 @@
-chrome.storage.local.get("bwActivated").then((result) => {
-    if (result && result.bwActivated) {
-        document.documentElement.style.filter = 'grayscale(100%)';
-    }
-});
+// chrome.storage.local.get("bwActivated").then((result) => {
+//     if (result && result.bwActivated) {
+//         document.documentElement.style.filter = 'grayscale(100%)';
+//     }
+// });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'convert') {
-        document.documentElement.style.filter = 'grayscale(100%)';
-        sendResponse({ message: 'Website converted to black and white.' });
-        chrome.storage.local.set({ "bwActivated": true });
-    } else if (request.action === 'reset') {
-        document.documentElement.style.filter = 'none';
-        sendResponse({ message: 'Website reset to original colors.' });
-        chrome.storage.local.set({ "bwActivated": false });
-    }
-});
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     if (request.action === 'convert') {
+//         document.documentElement.style.filter = 'grayscale(100%)';
+//         sendResponse({ message: 'Website converted to black and white.' });
+//         chrome.storage.local.set({ "bwActivated": true });
+//     } else if (request.action === 'reset') {
+//         document.documentElement.style.filter = 'none';
+//         sendResponse({ message: 'Website reset to original colors.' });
+//         chrome.storage.local.set({ "bwActivated": false });
+//     }
+// });
+
+// let tab = chrome.tabs.query({ active: true, currentWindow: true });
+
+// chrome.storage.local.onChanged.addListener(async (tab) => {
+    
+//     chrome.storage.local.get("bwActivated").then((result) => {
+//         if (result && result.bwActivated) {
+//             chrome.scripting.insertCSS({
+//                 target: {
+//                     tabId: tab.id,
+//                 },
+//                 files: ["test.css"],
+//             });
+//         }
+//         else {
+//             chrome.scripting.removeCSS({
+//                 target: {
+//                     tabId: tab.id,
+//                 },
+//                 files: ["test.css"],
+//             });
+//         }
+//     });
+// })
--- popup.js
+++ popup.js
@@ -1,28 +1,43 @@
+// const test = () => {
+//     chrome.storage.local.get("bwActivated").then((result) => {
+//         if (result && result.bwActivated) {
+//             document.documentElement.style.filter = 'grayscale(100%)';
+//         }
+//     });
+// }
+
 document.addEventListener('DOMContentLoaded', function () {
     var convertButton = document.getElementById('convertButton');
     var resetButton = document.getElementById('resetButton');
+    // let tab = chrome.tabs.query({ active: true, currentWindow: true });
 
     convertButton.addEventListener('click', function () {
+        chrome.storage.local.set({ "bwActivated": true });
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'convert' });
+            let file = tabs[0].url.includes("m.youtube") ? ["mobile.css"] : ["desktop.css"];
             chrome.scripting.insertCSS({
                 target: {
                     tabId: tabs[0].id,
                 },
-                files: ["test.css"],
+                files: file,
             });
         });
     });
 
     resetButton.addEventListener('click', function () {
+        chrome.storage.local.set({ "bwActivated": false });
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'reset' });
+            let file = tabs[0].url.includes("m.youtube") ? ["mobile.css"] : ["desktop.css"];
             chrome.scripting.removeCSS({
                 target: {
                     tabId: tabs[0].id,
                 },
-                files: ["test.css"],
+                files: file,
             });
         });
     });
+
+    // chrome.storage.local.addEventListener(() => {
+
+    // })
 });
